7c10df100e11241eec304b68c9815ff6b4ab6aa8
Georgi <georgi.penchev@endava.com>
2022-08-24 14:23:09 +0300

    order service implemented

---
+++ OrderServices/Database/DataBaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderServices.Database.Entities;
+
+namespace OrderServices.Database
+{
+    public class DataBaseContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=EN719380\\MSSQLSERVER01;Initial Catalog=OrderService;Trusted_Connection=True;MultipleActiveResultSets=true;");
+        }
+    }
+}
